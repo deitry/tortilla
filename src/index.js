291cdd663ac2bd4d5db98df64136451e47a52d7a
Dmitriy Vornychev <dm.s.vornychev@gmail.com>
2019-12-20 01:06:27 +0300

    Общее состояние доски + обработчик нажатия

--- src/index.js
+++ src/index.js
@@ -4,12 +4,6 @@ import './index.css';
 
 
 class Square extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: null,
-    }
-  }
 
   render() {
     return (
@@ -18,17 +12,39 @@ class Square extends React.Component {
 
       <button
         className="square"
-        onClick={() => this.setState({ value: 'X' })}
+        onClick={() => this.props.onClick()}
       >
-        {this.state.value}
+        {this.props.value}
       </button>
     );
   }
 }
 
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+    }
+  }
+
+  // NOTE: Соглашение об именах:
+  // onEvent для пропсов
+  // handleEvent для обработчиков
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({squares: squares});
+  }
+
   renderSquare(i) {
-    return <Square value={i} />;
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
   }
 
   // циклы в React
